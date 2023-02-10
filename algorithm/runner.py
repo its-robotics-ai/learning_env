5365b31421bc749bdf6ada703ce6a6635769c50d
h199001105523 <heejin_ju@hyundai-autoever.com>
2023-02-10 13:15:40 +0900

    feature : runner inference 수정 - colab 의 rest api 로 ai serving model inference 하도록 수정

--- algorithm/runner.py
+++ algorithm/runner.py
@@ -6,19 +6,38 @@ import onnxruntime
 import torch
 from lane_detector import LaneDetector
 import time
-# from jetbot import Robot
+import requests
+from jetbot import Robot
 
 
 class Runner:
-    def __init__(self, model_path):
+    def __init__(self, model_path, use_external, external_url):
         self.model_path = model_path
         self.detector = LaneDetector()
         self.session = onnxruntime.InferenceSession(model_path)
+        self.use_external = use_external
+        self.external_url = external_url
 
     def inference(self, x):
+        if self.use_external:
+            return self.inference_external(x)
+        else:
+            return self.inference_internal(x)
+
+    def inference_internal(self, x):
         ort_inputs = {self.session.get_inputs()[0].name: x}
+
         ort_outs = self.session.run(None, ort_inputs)
-        return ort_outs
+
+        left_speed = round(float(ort_outs[2][0][0]), 1)
+        right_speed = round(float(ort_outs[2][0][1]), 1)
+
+        return {"left_speed": left_speed, "right_speed": right_speed}
+
+    def inference_external(self, x):
+        x_centers = " ".join(str(s) for s in x[0])
+        response = requests.post(self.external_url, json={"centers": x_centers})
+        return response.json()
 
     def show_model_info(self):
         onnx_model = onnx.load(self.model_path)
@@ -36,11 +55,15 @@ class Runner:
 if __name__ == '__main__':
     args = sys.argv
 
-    if len(args) != 2:
-        print('Usage: runner.py {}'.format('{onnx_model_path}'))
+    if len(args) < 2:
+        print('Usage: runner.py {} {} {}'.format('{onnx_model_path}', '{external_url}'))
         sys.exit(1)
 
-    runner = Runner(args[1])
+    # use_external = False if args[2] is None else True
+    # external_url = args[2] if use_external else None
+    runner = Runner(args[1],
+                    False if args[2] is None else True,
+                    None if args[2] is None else args[2])
     detector = LaneDetector()
     cap = cv2.VideoCapture('./inputs/input.mp4')
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -54,14 +77,13 @@ if __name__ == '__main__':
         img_detect, x_center = detector.detect(img, False)
         x_center = x_center[0:8].astype('float32').reshape(1, 8)
         tensor_x_center = torch.Tensor(x_center)
+        # output = runner.inference_internal(x_center)
         output = runner.inference(x_center)
 
-        left_action = round(float(output[2][0][0]), 1)
-        right_action = round(float(output[2][0][1]), 1)
-
-        print(f'left: {left_action}, right: {right_action}')
-        # robot.left(speed=left_action)
-        # robot.right(speed=right_action)
+        print(f'center_input: {x_center}')
+        print(f'left: {output["left_speed"]}, right: {output["right_speed"]}')
+        robot.left(speed=left_action)
+        robot.right(speed=right_action)
 
         time.sleep(2)
 
